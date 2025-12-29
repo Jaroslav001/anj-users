b1c1d8c3f4b409c8830b4f7eda9e4957ad41906f
Jaroslav001 <jaroslav.rohal@gmail.com>
2025-12-29 17:55:47 +0100

    Fix defaults

--- inc/admin/new-user-defaults.php
+++ inc/admin/new-user-defaults.php
@@ -59,6 +59,45 @@ add_filter('get_user_option_show_admin_bar_front', function ($value, $option, $u
 	return $value;
 }, 20, 3);
 
+/**
+ * Admin UI: default the "Send User Notification" checkbox to unchecked.
+ *
+ * WordPress renders this checkbox checked by default on user-new.php (id: send_user_notification).
+ * We flip it off on the Add New User screen so admins must opt-in to sending the email.
+ */
+function anj_users_uncheck_send_user_notification_checkbox_js()
+{
+	if (!is_admin()) {
+		return;
+	}
+
+?>
+	<script>
+		(function() {
+			function uncheck() {
+				var el = document.getElementById('send_user_notification');
+				if (!el) return;
+				el.checked = false;
+				el.defaultChecked = false;
+			}
+			if (document.readyState === 'loading') {
+				document.addEventListener('DOMContentLoaded', uncheck);
+			} else {
+				uncheck();
+			}
+			window.addEventListener('load', uncheck);
+			setTimeout(uncheck, 50);
+			setTimeout(uncheck, 250);
+			setTimeout(uncheck, 1000);
+		})();
+	</script>
+<?php
+}
+
+add_action('admin_print_footer_scripts-user-new.php', 'anj_users_uncheck_send_user_notification_checkbox_js');
+add_action('network_admin_print_footer_scripts-user-new.php', 'anj_users_uncheck_send_user_notification_checkbox_js');
+
+
 // Multisite: new user created in Network Admin.
 add_action('wpmu_new_user', function ($user_id) {
 	$user_id = (int) $user_id;
